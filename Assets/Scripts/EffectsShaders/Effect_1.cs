888bd1848c0e8134689d6113d6ff223e7d033fcf
Segoo <68073260+Michikatsu0@users.noreply.github.com>
2023-05-20 03:27:50 -0500

    a

--- Assets/Scripts/EffectsShaders/Effect_1.cs
+++ Assets/Scripts/EffectsShaders/Effect_1.cs
@@ -8,13 +8,19 @@ public class Effect_1 : BaseShaderController
     // SwitcherEffect.Instance.colorEffect; para acceder al color del slider
     public ParticleSystem[] pSystem;
 
-    void Start()
-    {
-        
-    }
-
     void Update()
     {
-
+        foreach (var ps in pSystem)
+        {
+            var main = ps.main;
+            main.startColor = SwitcherEffect.Instance.colorEffect1;
+        }
+        shaderMaterial[0].SetColor("_Color", SwitcherEffect.Instance.colorEffect1);
+        
+        if (float.TryParse(uIVariables[0].text, out float result))
+        {
+            var mian = pSystem[pSystem.Length - 2].main;
+            mian.startSize = result;
+        }
     }
 }
